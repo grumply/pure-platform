6548a9514ba0a9dd6c784530c8a14258d66c3e32
grumply <sean@grump.ly>
2018-06-11 22:07:19 -0500

    Bump pure-core, pure-dom, pure-lifted, and pure. Update benchmark.

--- js-framework-benchmark/Main.hs
+++ js-framework-benchmark/Main.hs
@@ -1,36 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+-- from pure
+import Pure
+
+-- from pure-txt
+import qualified Pure.Data.Txt as Txt
+
 -- from base
 import Control.Arrow ((&&&))
 import Control.Monad (void)
 import qualified Data.List as L (tail,splitAt,filter)
 import Data.Monoid ((<>))
 import Data.Traversable (for)
-
--- from pure-default
-import Pure.Data.Default (Default(..))
-
--- from pure-txt
-import Pure.Data.Txt (Txt,ToTxt(..),FromTxt(..))
-import qualified Pure.Data.Txt as Txt
-
--- from pure-core
-import Pure.Data.View
-import Pure.Data.View.Patterns
-
--- from pure-html
-import Pure.Data.HTML
-import Pure.Data.HTML.Properties hiding (Span)
-
--- from pure-dom
-import Pure.DOM (inject)
-
--- from pure-lifted
-import Pure.Data.Lifted (Element(..),Body(..),getBody,toJSV)
-
--- from pure-events
-import Pure.Data.Events
+import Data.Function (fix)
 
 -- from vector
 import qualified Data.Vector as V
@@ -43,13 +26,28 @@ foreign import javascript unsafe
     rand :: Int -> IO Int
 
 data Row = Row
-  { ident    :: Int
-  , label    :: Txt
-  , selected :: Bool
-  } deriving Eq
+  { ident     :: Int
+  , label     :: String
+  , selected  :: Bool
+  , update    :: Msg -> IO ()
+  , rendered  :: View
+  }
+
+instance Pure Row where
+  view (Row i l s upd _) =
+    Tr <| (if s then Class "danger" else id) |>
+      [ Td <| Class "col-md-1" |> [ fromTxt $ int i ]
+      , Td <| Class "col-md-4" |> [ A <| OnClick (\_ -> upd (SelectM i)) |> [ fromTxt $ toTxt l ] ]
+      , Td <| Class "col-md-1" |>
+          [ A <| OnClick (\_ -> upd (RemoveM i)) |>
+              [ Span <| Classes [ "glyphicon", "glyphicon-remove" ] . Attribute "aria-hidden" "true"
+              ]
+          ]
+      , Td <| Class "col-md-6"
+      ]
 
 data Model = Model
-  { rows    :: [(Int,Row)]
+  { rows    :: V.Vector (Int,Row)
   , lastId  :: Int
   }
 
@@ -63,7 +61,7 @@ data Msg
   | RemoveM Int
   | DoNothingM
 
-adjectives :: V.Vector Txt
+adjectives :: V.Vector String
 adjectives = V.fromList
   [  "pretty",  "large",  "big",  "small",  "tall",  "short",  "long"
   ,  "handsome",  "plain",  "quaint",  "clean",  "elegant",  "easy",  "angry"
@@ -71,13 +69,13 @@ adjectives = V.fromList
   ,  "important",  "inexpensive",  "cheap",  "expensive",  "fancy"
   ]
 
-colors :: V.Vector Txt
+colors :: V.Vector String
 colors = V.fromList
   [  "red",  "yellow",  "blue",  "green",  "pink",  "brown"
   ,  "purple",  "brown",  "white",  "black",  "orange"
   ]
 
-nouns :: V.Vector Txt
+nouns :: V.Vector String
 nouns = V.fromList
   [  "table",  "chair",  "house",  "bbq",  "desk",  "car"
   ,  "pony",  "cookie",  "sandwich",  "burger",  "pizza"
@@ -90,42 +88,39 @@ text = fromTxt . toTxt
 choose :: V.Vector x -> IO x
 choose vs = do
   r <- rand (V.length vs)
-  return $ vs V.! r
+  return $ V.unsafeIndex vs r
 
-createRows :: Int -> Int -> IO [(Int,Row)]
-createRows n newId = for [0..n] $ \n -> do
+createRows :: Int -> Int -> (Msg -> IO ()) -> IO (V.Vector (Int,Row))
+createRows n newId upd = V.generateM n $ \n -> do
   adjective <- choose adjectives
   color     <- choose colors
   noun      <- choose nouns
-  return (n,Row
-    { ident    = newId + n
-    , label    = Txt.intercalate " " [adjective,color,noun]
-    , selected = False
-    })
+  return (n,fix $ \r -> Row (newId + n) (adjective <> " " <> color <> " " <> noun) False upd (view r))
+
+-- bang :: (Int,Row) -> (Int,Row)
+-- bang (index,row)
+--   | index `mod` 10 == 0 = (index,fix $ \r -> row { label = label row <> " !!!", rendered = view r })
+--   | otherwise           = (index,row)
 
 bang :: (Int,Row) -> (Int,Row)
-bang (index,row)
-  | index `mod` 10 == 0 = (index,row { label = label row <> " !!!" })
-  | otherwise           = (index,row)
+bang (index,row) = (index,fix $ \r -> row { label = label row <> " !!!", rendered = view r })
 
--- updateEvery :: Int -> (Row -> Row) -> [Row] -> [Row]
--- updateEvery n f = V.toList . update . V.fromList
---   where
---     update vector =
---       let
---         count = quot (V.length vector) n
---         patch x = ( x * n, f )
---         patches = V.generate count patch
---       in
---         V.accumulate (flip ($)) vector patches
+updateEvery :: Int -> (a -> a) -> V.Vector a -> V.Vector a
+updateEvery n f vector =
+    let
+      count = quot (V.length vector) n
+      patch x = ( x * n, f )
+      patches = V.generate count patch
+    in
+      V.accumulate (flip ($)) vector patches
 
-swap :: Int -> Int -> [a] -> [a]
-swap i j = V.toList . V.modify (\v -> MV.swap v i j) . V.fromList
+swap :: Int -> Int -> V.Vector a -> V.Vector a
+swap i j = V.modify (\v -> MV.swap v i j)
 
 selectRow :: Int -> Row -> Row
 selectRow i row
-  | i == ident row = row { selected = True  }
-  | i /= ident row = row { selected = False }
+  | i == ident row = fix $ \r -> row { selected = True , rendered = view r }
+  | selected row   = fix $ \r -> row { selected = False, rendered = view r }
   | otherwise      = row
 
 main :: IO ()
@@ -137,24 +132,24 @@ main = do
           mdl' <-
             case msg of
               CreateM amount -> do
-                newRows <- createRows amount (lastId mdl)
+                newRows <- createRows amount (lastId mdl) upd
                 return mdl { rows = newRows, lastId = lastId mdl + amount }
 
               AppendM amount -> do
-                newRows <- createRows amount (lastId mdl)
+                newRows <- createRows amount (lastId mdl) upd
                 return mdl { rows = rows mdl <> newRows, lastId = lastId mdl + amount }
 
               UpdateEveryM amount ->
-                return mdl { rows = fmap bang (rows mdl) }
+                return mdl { rows = updateEvery 10 bang (rows mdl) }
 
               ClearM ->
-                return mdl { rows = [] }
+                return mdl { rows = V.empty }
 
               SwapM ->
                 return mdl { rows = swap 1 998 (rows mdl) }
 
               RemoveM i ->
-                return mdl { rows = L.filter (\(_,r) -> ident r /= i) (rows mdl) }
+                return mdl { rows = V.filter (\(_,r) -> ident r /= i) (rows mdl) }
 
               SelectM i ->
                 return mdl { rows = fmap (fmap (selectRow i)) (rows mdl) }
@@ -190,28 +185,18 @@ main = do
               , ( "donothing", "Do Nothing", DoNothingM )
               ]
 
-        row :: Row -> View
-        row = ComponentIO $ \self ->
-          def
-            { construct = return ()
-            , force = \n _ -> do
-                o <- getProps self
-                return (ident o /= ident n || label o /= label n || selected o /= selected n)
-            , render = \(Row i l s) _ ->
-                Tr <| (if s then Class "danger" else id) |>
-                  [ Td <| Class "col-md-1" |> [ text i ]
-                  , Td <| Class "col-md-4" |> [ A <| OnClick (\_ -> upd (SelectM i)) |> [ text l ] ]
-                  , Td <| Class "col-md-1" |>
-                      [ A <| OnClick (\_ -> upd (RemoveM i)) |>
-                          [ Span <| Classes [ "glyphicon", "glyphicon-remove" ] . Attribute "aria-hidden" "true"
-                          ]
-                      ]
-                  , Td <| Class "col-md-6"
-                  ]
-            }
+        -- row :: Row -> View
+        -- row = ComponentIO $ \self ->
+        --   def
+        --     { construct = return ()
+        --     , force = \n _ -> do
+        --         o <- getProps self
+        --         return (ident o /= ident n || label o /= label n || selected o /= selected n)
+        --     , render = \
+        --     }
     in
         def
-            { construct = return (Model [] 1)
+            { construct = return (Model V.empty 1)
             , render = \_ model ->
                 Div <| Id "main" |>
                     [ Div <| Class "container" |>
@@ -222,7 +207,7 @@ main = do
                                   ]
                             ]
                         , Table <| Classes [ "table", "table-hover", "table-striped", "test-data" ] |>
-                            [ (Keyed Tbody) <||#> fmap (fmap row) (rows model) ]
+                            [ (Keyed Tbody) <||#> fmap (fmap rendered) (V.toList (rows model)) ]
                         , Span <| Classes [ "preloadicon", "glyphicon", "glyphicon-remove" ] . Property "aria-hidden" "true"
                         ]
                     ]
