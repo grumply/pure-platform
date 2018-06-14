10a547d09f72494bf54c38a47cc9cff0c38e00bb
grumply <sean@grump.ly>
2018-06-13 19:54:29 -0500

    Bump pure-core, pure-css, pure-dom, and pure-render. Refine js-framework-benchmark.

--- js-framework-benchmark/Main.hs
+++ js-framework-benchmark/Main.hs
@@ -4,16 +4,12 @@ module Main where
 -- from pure
 import Pure
 
--- from pure-txt
-import qualified Pure.Data.Txt as Txt
-
 -- from base
 import Control.Arrow ((&&&))
-import Control.Monad (void)
-import qualified Data.List as L (tail,splitAt,filter)
+import Control.Monad (unless,void)
+import Data.List as L (filter)
 import Data.Monoid ((<>))
 import Data.Traversable (for)
-import Data.Function (fix)
 
 -- from vector
 import qualified Data.Vector as V
@@ -33,21 +29,8 @@ data Row = Row
   , rendered  :: View
   }
 
-instance Pure Row where
-  view (Row i l s upd _) =
-    Tr <| (if s then Class "danger" else id) |>
-      [ Td <| Class "col-md-1" |> [ fromTxt $ int i ]
-      , Td <| Class "col-md-4" |> [ A <| OnClick (\_ -> upd (SelectM i)) |> [ fromTxt $ toTxt l ] ]
-      , Td <| Class "col-md-1" |>
-          [ A <| OnClick (\_ -> upd (RemoveM i)) |>
-              [ Span <| Classes [ "glyphicon", "glyphicon-remove" ] . Attribute "aria-hidden" "true"
-              ]
-          ]
-      , Td <| Class "col-md-6"
-      ]
-
 data Model = Model
-  { rows    :: V.Vector (Int,Row)
+  { rows    :: V.Vector Row
   , lastId  :: Int
   }
 
@@ -59,7 +42,6 @@ data Msg
   | SwapM
   | SelectM Int
   | RemoveM Int
-  | DoNothingM
 
 adjectives :: V.Vector String
 adjectives = V.fromList
@@ -82,51 +64,93 @@ nouns = V.fromList
   ,  "mouse",  "keyboard"
   ]
 
-text :: (ToTxt a) => a -> View
-text = fromTxt . toTxt
-
 choose :: V.Vector x -> IO x
 choose vs = do
   r <- rand (V.length vs)
   return $ V.unsafeIndex vs r
 
-createRows :: Int -> Int -> (Msg -> IO ()) -> IO (V.Vector (Int,Row))
-createRows n newId upd = V.generateM n $ \n -> do
+createRows :: Int -> Int -> (Msg -> IO ()) -> IO (V.Vector Row)
+createRows n newId update = V.generateM n $ \n -> do
   adjective <- choose adjectives
   color     <- choose colors
   noun      <- choose nouns
-  return (n,fix $ \r -> Row (newId + n) (adjective <> " " <> color <> " " <> noun) False upd (view r))
+  return $ renderRow $ Row (newId + n) (adjective <> " " <> color <> " " <> noun) False update undefined
 
--- bang :: (Int,Row) -> (Int,Row)
--- bang (index,row)
---   | index `mod` 10 == 0 = (index,fix $ \r -> row { label = label row <> " !!!", rendered = view r })
---   | otherwise           = (index,row)
-
-bang :: (Int,Row) -> (Int,Row)
-bang (index,row) = (index,fix $ \r -> row { label = label row <> " !!!", rendered = view r })
+bang :: Row -> Row
+bang row = renderRow row { label = label row <> " !!!" }
 
 updateEvery :: Int -> (a -> a) -> V.Vector a -> V.Vector a
-updateEvery n f vector =
-    let
-      count = quot (V.length vector) n
-      patch x = ( x * n, f )
-      patches = V.generate count patch
-    in
-      V.accumulate (flip ($)) vector patches
+updateEvery n f = V.modify (flip update 0)
+  where
+    update v = go
+      where
+        go x = unless (x >= MV.length v) $ do
+          MV.unsafeModify v f x
+          go (x + n)
 
 swap :: Int -> Int -> V.Vector a -> V.Vector a
 swap i j = V.modify (\v -> MV.swap v i j)
 
+renderRow :: Row -> Row
+renderRow r = r { rendered = View r }
+
 selectRow :: Int -> Row -> Row
 selectRow i row
-  | i == ident row = fix $ \r -> row { selected = True , rendered = view r }
-  | selected row   = fix $ \r -> row { selected = False, rendered = view r }
+  | i == ident row = renderRow row { selected = True  }
+  | selected row   = renderRow row { selected = False }
   | otherwise      = row
+
+data Button = Button
+  { bId :: Txt
+  , bLabel :: Txt
+  , bEvt :: IO ()
+  }
+
+button :: (Msg -> IO ()) -> (Txt,Txt,Msg) -> View
+button f (ident,label,msg) = View (Main.Button ident label (f msg))
+
+instance Pure Main.Button where
+  view (Main.Button ident label evt) =
+    Div <| Classes [ "col-sm-6", "smallpad" ] |>
+        [ Pure.Button <| Attribute "ref" "text"
+                      . Classes [ "btn", "btn-primary", "btn-block" ]
+                      . OnClick (\_ -> evt)
+                      . Type "button"
+                      . Id ident
+                      |>
+            [ txt label ]
+        ]
+
+buttons :: [(Txt,Txt,Msg)]
+buttons =
+    [ ( "run"       , "Create 1,000 rows"    , CreateM 1000      )
+    , ( "runlots"   , "Create 10,000 rows"   , CreateM 10000     )
+    , ( "add"       , "Append 1,000 rows"    , AppendM 1000      )
+    , ( "update"    , "Update every 10th row", UpdateEveryM 10   )
+    , ( "clear"     , "Clear"                , ClearM            )
+    , ( "swaprows"  , "Swap Rows"            , SwapM             )
+    ]
+
+instance Pure Row where
+  view (Row i l s upd _) =
+    Tr <| (if s then Class "danger" else id) |>
+      [ Td <| Class "col-md-1" |> [ text i ]
+      , Td <| Class "col-md-4" |> [ A <| OnClick (\_ -> upd (SelectM i)) |> [ text l ] ]
+      , Td <| Class "col-md-1" |>
+          [ A <| OnClick (\_ -> upd (RemoveM i)) |>
+              [ Span <| Classes [ "glyphicon", "glyphicon-remove" ] . Attribute "aria-hidden" "true"
+              ]
+          ]
+      , Td <| Class "col-md-6"
+      ]
+
+keyedRows :: V.Vector Row -> [(Int,View)]
+keyedRows = fmap (ident &&& rendered) . V.toList
 
 main :: IO ()
 main = do
   b <- getBody
-  inject (Element $ toJSV b) $ ($ ()) $ ComponentIO $ \self ->
+  inject b $ flip ComponentIO () $ \self ->
     let
         upd msg = void $ setState self $ \_ mdl -> do
           mdl' <-
@@ -149,51 +173,13 @@ main = do
                 return mdl { rows = swap 1 998 (rows mdl) }
 
               RemoveM i ->
-                return mdl { rows = V.filter (\(_,r) -> ident r /= i) (rows mdl) }
+                return mdl { rows = V.filter ((/= i) . ident) (rows mdl) }
 
               SelectM i ->
-                return mdl { rows = fmap (fmap (selectRow i)) (rows mdl) }
+                return mdl { rows = fmap (selectRow i) (rows mdl) }
 
-              DoNothingM ->
-                return mdl
           return (mdl',return ())
 
-        buttonPrimaryBlock :: [View]
-        buttonPrimaryBlock = fmap button buttons
-          where
-            button (ident,label,msg) =
-              Div <| Classes [ "col-sm-6", "smallpad" ] |>
-                [ Button <| Attribute "ref" "text"
-                          . Classes [ "btn", "btn-primary", "btn-block" ]
-                          . OnClick (\_ -> upd msg)
-                          . Type "button"
-                          . Id ident
-                          |>
-                    [ fromTxt label ]
-                ]
-
-            buttons =
-              [ ( "run10", "Create 10 rows", CreateM 10 )
-              , ( "run", "Create 1,000 rows", CreateM 1000 )
-              , ( "runlots", "Create 10,000 rows", CreateM 10000 )
-              , ( "add10", "Append 100 rows", AppendM 10 )
-              , ( "add", "Append 1,000 rows", AppendM 1000 )
-              , ( "update2", "Update every 2nd row", UpdateEveryM 2 )
-              , ( "update", "Update every 10th row", UpdateEveryM 10 )
-              , ( "clear", "Clear", ClearM )
-              , ( "swaprows", "Swap Rows", SwapM )
-              , ( "donothing", "Do Nothing", DoNothingM )
-              ]
-
-        -- row :: Row -> View
-        -- row = ComponentIO $ \self ->
-        --   def
-        --     { construct = return ()
-        --     , force = \n _ -> do
-        --         o <- getProps self
-        --         return (ident o /= ident n || label o /= label n || selected o /= selected n)
-        --     , render = \
-        --     }
     in
         def
             { construct = return (Model V.empty 1)
@@ -202,12 +188,12 @@ main = do
                     [ Div <| Class "container" |>
                         [ Div <| Class "jumbotron" |>
                             [ Div <| Class "row" |>
-                                  [ Div <| Class "col-md-6" |> [ H1 <||> [ "pure-v0.7-keyed" ] ]
-                                  , Div <| Class "col-md-6" |> buttonPrimaryBlock
+                                  [ Div <| Class "col-md-6" |> [ H1 <||> [ txt "pure-v0.7-keyed" ] ]
+                                  , Div <| Class "col-md-6" |> (fmap (Main.button upd) buttons)
                                   ]
                             ]
                         , Table <| Classes [ "table", "table-hover", "table-striped", "test-data" ] |>
-                            [ (Keyed Tbody) <||#> fmap (fmap rendered) (V.toList (rows model)) ]
+                            [ Keyed Tbody <||#> keyedRows (rows model) ]
                         , Span <| Classes [ "preloadicon", "glyphicon", "glyphicon-remove" ] . Property "aria-hidden" "true"
                         ]
                     ]
