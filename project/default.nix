27c006bdf14f3231af6a9902f07de7cc17144eb3
Sean Hickman <sean@grumply.com>
2021-09-25 15:55:51 -0500

    Add pure-sorcerer.

--- project/default.nix
+++ project/default.nix
@@ -211,6 +211,7 @@ let
     pure-router = this;
     pure-scroll-loader = this;
     pure-search = this;
+    pure-sorcerer = this;
     pure-spacetime = this;
     pure-spinners = this;
     pure-state = this;
